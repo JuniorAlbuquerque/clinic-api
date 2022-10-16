a9bb40ad389ec46d818c6cbbed65b4fad3d09a0f
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2022-10-16 20:20:31 -0300

    chore: add vercel config

---
+++ dist/config/auth/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: 'fb8a95c8ecbe10781666e23701a9ae02',
+        expiresIn: '5d'
+    }
+};
